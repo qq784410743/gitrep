b26f61bbd77bd3b0fb8a2c00e5d4982cad049938
guozhuang <784410743@qq.com>
2019-04-22 10:54:59 +0800

    第二周周考sdf

---
+++ ashds/src/main/java/com/guozhuangzhuang/common/utils/ashds/App.java
@@ -0,0 +1,13 @@
+package com.guozhuangzhuang.common.utils.ashds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
